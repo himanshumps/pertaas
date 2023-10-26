25e595e611e695b3c8bccd61b58dd5a8ba394685
Himanshu Gupta <himanshu.mps@gmail.com>
2023-10-26 14:45:23 +0530

    Sample healthz controller

---
+++ pertaas-api/src/main/java/org/redhat/hackathon/HealthzController.java
@@ -0,0 +1,16 @@
+package org.redhat.hackathon;
+
+import io.vertx.core.json.JsonObject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+
+@Path("/healthz")
+public class HealthzController {
+    @GET
+    public CompletionStage<String> getHealthz() {
+        return CompletableFuture.completedFuture(JsonObject.of("success", true).encode());
+    }
+}
