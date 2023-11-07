dfe494792e7d7b82162268c2587a53e7fcfcc91e
Himanshu Gupta <himanshu.gupta25@aexp.com>
2023-11-07 11:35:01 +0530

    Job Controller

--- pertaas-api/src/main/java/org/redhat/hackathon/job/JobController.java
+++ pertaas-api/src/main/java/org/redhat/hackathon/job/JobController.java
@@ -1,61 +1,79 @@
 package org.redhat.hackathon.job;
 
+import com.couchbase.client.java.Bucket;
+import com.couchbase.client.java.kv.CounterResult;
+import com.couchbase.client.java.kv.IncrementOptions;
 import io.fabric8.kubernetes.api.model.EmptyDirVolumeSource;
-import io.fabric8.kubernetes.client.KubernetesClientBuilder;
-import io.fabric8.openshift.client.DefaultOpenShiftClient;
-import io.fabric8.openshift.client.OpenShiftClient;
 import io.fabric8.tekton.client.TektonClient;
-import io.fabric8.tekton.pipeline.v1.*;
-import io.fabric8.tekton.pipeline.v1alpha1.ArrayOrString;
-import io.fabric8.tekton.triggers.v1alpha1.TriggerTemplateBuilder;
+import io.fabric8.tekton.pipeline.v1.ParamValue;
+import io.fabric8.tekton.pipeline.v1.PipelineRun;
+import io.fabric8.tekton.pipeline.v1.PipelineRunBuilder;
+import io.fabric8.tekton.pipeline.v1.WorkspaceBindingBuilder;
+import io.quarkus.logging.Log;
+import io.smallrye.common.annotation.RunOnVirtualThread;
 import io.vertx.core.json.JsonObject;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.QueryParam;
-
-import java.util.Collections;
-import java.util.List;
 
 @Path("/job")
 public class JobController {
 
-  @Inject
+
   TektonClient tektonClient;
 
-  @GET
-  @Path("/create")
-  public String createJobViaTektonPipeline() {
-    /*JsonObject jsonObject = new JsonObject(jsonString);
-    int stepDuration = jsonObject.getInteger("stepDuration");
-    String gitUrl = jsonObject.getString("gitUrl");
-    String revision = jsonObject.getString("revision");
-    JsonObject requestJson = jsonObject.getJsonObject("requestJson");*/
+  @Inject
+  Bucket bucket;
 
+  @POST
+  @Path("/create")
+  @RunOnVirtualThread
+  public String createJobViaTektonPipeline(String jsonString) {
+    Log.info("Received: " + jsonString);
+    CounterResult counterResult = bucket.defaultCollection().binary().increment("JOB_COUNTER", IncrementOptions.incrementOptions().initial(1));
+    String jobId = "job-" +  Long.valueOf(counterResult.content()).intValue();
+    JsonObject jsonObject = new JsonObject(jsonString);
+    String stepDuration = jsonObject.getString("stepDuration", "10");
+    String github_url = jsonObject.getString("github_url");
+    String revision = jsonObject.getString("revision");
+    Object requestJson = jsonObject.getValue("request_json");
+    String cpu = jsonObject.getString("cpu", "1000");
+    String ram = jsonObject.getString("ram", "1024");
     PipelineRun pipelineRunBuilderSpecNested = new PipelineRunBuilder()
-        .withNewMetadata().withName("test-pipeline-run")
+        .withNewMetadata().withName(jobId + "-run")
         .endMetadata()
         .withNewSpec()
         .withNewPipelineRef()
         .withName("job-pipeline")
         .endPipelineRef()
-
         .addNewParam()
         .withName("jobId")
-        .withValue(new ParamValue("job-17"))
+        .withValue(new ParamValue(jobId))
         .endParam()
         .addNewParam()
         .withName("requestJson")
-        .withValue(new ParamValue("{\"hostname\":\"pertaas-api-himanshumps-1-dev.apps.sandbox-m3.1530.p1.openshiftapps.com\",\"port\":443,\"ssl\":true,\"maxConnections\":10,\"runDurationInSeconds\":180,\"requestPerSecond\":10000000,\"httpRequests\":[{\"method\":\"GET\",\"uri\":\"/healthz\",\"headers\":null,\"queryParams\":null,\"pathParams\":null,\"body\":null}]}"))
+        .withValue(new ParamValue(requestJson.toString()))
         .endParam()
         .addNewParam()
         .withName("gitRepoUrl")
-        .withValue(new ParamValue("https://github.com/himanshumps/pertaas-job.git"))
+        .withValue(new ParamValue(github_url))
         .endParam()
         .addNewParam()
         .withName("gitRevision")
-        .withValue(new ParamValue("main"))
+        .withValue(new ParamValue(revision))
+        .endParam()
+        .addNewParam()
+        .withName("stepDuration")
+        .withValue(new ParamValue(stepDuration))
+        .endParam()
+        .addNewParam()
+        .withName("cpu")
+        .withValue(new ParamValue(cpu))
+        .endParam()
+        .addNewParam()
+        .withName("ram")
+        .withValue(new ParamValue(ram))
         .endParam()
         .withWorkspaces(new WorkspaceBindingBuilder().withName("emptydir").withNewPersistentVolumeClaim("tekton1", false).build(),
             new WorkspaceBindingBuilder().withName("configmap_settings").withEmptyDir(new EmptyDirVolumeSource()).build())
