40950617e152dfec2a05b7ce4d5a6690c3ff4083
Himanshu Gupta <himanshu.mps@gmail.com>
2023-11-13 15:26:03 +0530

    Test cases related code

---
+++ pertaas-api/src/main/java/org/redhat/hackathon/controller/HealthzController.java
@@ -0,0 +1,35 @@
+package org.redhat.hackathon.controller;
+
+import io.vertx.core.json.JsonObject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+
+@Path("/healthz")
+public class HealthzController {
+    @GET
+    public CompletionStage<String> getHealthz() {
+        return CompletableFuture.completedFuture(JsonObject.of("success", true).encode());
+    }
+
+    @GET
+    @Path("/1")
+    public CompletionStage<String> getHealthz1() {
+        return CompletableFuture.completedFuture(JsonObject.of("success", true, "endpoint", "Healthz 1 endpoint").encode());
+    }
+
+    @GET
+    @Path("/2")
+    public CompletionStage<String> getHealthz2() {
+        return CompletableFuture.completedFuture(JsonObject.of("success", true, "endpoint", "Healthz 2 endpoint").encode());
+    }
+
+    @POST
+    public CompletionStage<String> postHealthz(String body) {
+        return CompletableFuture.completedFuture(JsonObject.of("success", true, "length", body != null ? body.length() : 0).encode());
+    }
+
+}
